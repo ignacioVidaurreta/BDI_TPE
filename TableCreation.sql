3d1b1144b560967cd6baef26159a88fc60b5fceb
Marcos <mlund@itba.edu.ar>
2018-11-08 12:12:59 -0300

    TableCreation.sql

---
+++ TableCreation.sql
@@ -0,0 +1,26 @@
+CREATE TABLE UserTP (
+        Id INT,
+        Reputation INT,
+        CreationDate TIMESTAMP NOT NULL,
+        DisplayName TEXT NOT NULL,
+        LastAccessDate TIMESTAMP NOT NULL,
+        WebsiteURL TEXT,
+        UserLocation TEXT,
+        AboutMe TEXT,
+        Views INT DEFAULT 0,
+        UpVotes INT DEFAULT 0,
+        DownVotes INT DEFAULT 0,
+        AccountID INT NOT NULL,
+        PRIMARY KEY(Id)
+);
+
+CREATE TABLE Badges (
+        Id INT,
+        UserId INT NOT NULL,
+        BadgeName TEXT NOT NULL,
+        BadgeDate TIMESTAMP NOT NULL,
+        BadgeClass INT NOT NULL,
+        TagBased BOOLEAN NOT NULL,
+        PRIMARY KEY(Id),
+        FOREIGN KEY(UserId) REFERENCES UserTP ON DELETE CASCADE
+);
